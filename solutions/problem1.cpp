549ea57a8bb293882f8c870f8bcdc845eeb69d59
Drew <145530440+DrewRoss5@users.noreply.github.com>
2026-02-05 11:13:12 -0700

    Optimize sum calculation for multiples of 3 and 5

--- solutions/problem1.cpp
+++ solutions/problem1.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 
 int main(int argc, char** argv){
-    int target {1000}, sum {0};
+    int target {1000}, sum {0}, curr {1};
     // initialize the target
     if (argc > 1)
         target = std::atoi(argv[1]);
     // find our solution
-    for (int i = 0; i < target; i++){
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
+    while (curr * 3 < target){
+        sum += (curr * 3);
+        if (curr < (target / 5) && curr * 5 % 15 > 0)
+            sum += (curr * 5);
+        curr++;
+
     }
     std::cout << "Result: " << sum << std::endl;
-    
-}
+} 
